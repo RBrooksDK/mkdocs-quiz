ee93c9260ff49cfc4f2ac066caf45bf027835455
RBrooksDK <richardbrooks.dk@gmail.com>
2025-05-08 19:01:08 +0200

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 
 setup(
     name='mkdocs_quiz',
-    version='0.0.42',
+    version='0.0.43',
     packages=['mkdocs_quiz',],
     package_data={'mkdocs_quiz': ['css/*', 'js/*']},
     include_package_data=True,
     license='Apache License 2.0',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/skyface753/mkdocs-quiz',
-    description='A mkdocs plugin to create a quiz in your markdown document.',
-    author='Sebastian Jörz',
-    author_email='sjoerz@skyface.de',
+    url='https://github.com/RBrooksDK/mkdocs-quiz',
+    description='A MkDocs plugin to create a quiz in your markdown document.',
+    author='Richard Brooks',
+    author_email='rib@via.dk',
     install_requires=[
         "mkdocs",
     ],
